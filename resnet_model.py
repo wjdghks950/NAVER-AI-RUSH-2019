3e19a2a6d656d813200c4e4ece393414c18dbdc9
Jeonghwan <jeonghwankim123@gmail.com>
2019-08-08 22:05:12 +0900

    normalize train set

--- resnet_model.py
+++ resnet_model.py
@@ -11,6 +11,9 @@ class Resnet(nn.Module):
         elif model_size == 101:
             model = models.resnet101(pretrained=True)
             expansion = 4
+        elif model_size == 50:
+            model = models.resnet50(pretrained=True)
+            expansion = 1
         model = list(model.children())[:-1]
         model.append(nn.Conv2d(512 * expansion, out_size, 1))
         self.net = nn.Sequential(*model)
