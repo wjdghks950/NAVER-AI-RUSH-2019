c551acbac3c40ad4da0b7a00e30c26459a36e3b6
korchris <jon6791@gmail.com>
2019-08-23 22:45:07 +0900

    add custom models

--- Round2/main.py
+++ Round2/main.py
@@ -98,14 +98,14 @@ class custom_model2(nn.Module):
             nn.Linear(60, 10),
         )
 
-        self.classifier = nn.Linear(110, 1)
+        self.classifier = nn.Linear(174, 1)
 
     def forward(self, image, eif, ff):
         _img=self.image_net(image).squeeze(-1).squeeze(-1)
         _eif=self.eif_net(eif)
         _ff=self.ff_net(ff)
         _image = torch.cat((_img, _eif), 1)
-        all_features = torch.cat((_eif, _ff), 1)
+        all_features = torch.cat((_image, _ff), 1)
         output = self.classifier(all_features)
 
         return output
