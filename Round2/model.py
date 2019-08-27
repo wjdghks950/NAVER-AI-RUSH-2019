f6c3f8784de9d9570ef3f89d2aaa22d7d0cba184
Jeonghwan <jeonghwankim123@gmail.com>
2019-08-27 16:52:55 +0900

    Fix size

--- Round2/model.py
+++ Round2/model.py
@@ -143,7 +143,7 @@ class custom_model3(nn.Module):
     def __init__(self, num_classes=1):
         super(custom_model3, self).__init__()
         self.num_classes = num_classes
-        
+
         self.eif_net = nn.Sequential(
             nn.Linear(2048, 2048),
             nn.ReLU(True),
@@ -180,6 +180,9 @@ class custom_model3(nn.Module):
         self.classifier = nn.Linear(210, self.num_classes)
 
     def forward(self, eif, ff, sequence):
+        print('[eif]:', eif)
+        print('[ff]:', ff)
+        print('[history]:', sequence)
         _eif=self.eif_net(eif)
         _ff=self.ff_net(ff)
         _history = self.history_net(sequence)
